8f5fb46cb68a69f5bb3d5c7ca1465c7bfd05d44a
Kumara1973 <152588563+Kumara1973@users.noreply.github.com>
2026-01-25 18:00:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let typeName = () => {
+    let name = document.getElementById("textbox").value;
+    document.getElementById("message").innerHTML = "Hi" + name;
+    
+}
+
+document.getElementById("bttn").addEventListener("click", typeName)
