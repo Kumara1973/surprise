cd3983e2931380b922247592240bf8c143061fab
Kumara1973 <152588563+Kumara1973@users.noreply.github.com>
2026-02-01 16:59:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let typeName = () => {
     let name = document.getElementById("textbox").value;
-    document.getElementById("message").innerHTML = "Hi" + name;
+    document.getElementById("message").innerHTML = "Hi " + name;
     
 }
 
