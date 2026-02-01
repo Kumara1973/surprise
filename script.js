4f0f3edd185b897837d09d15153f12fbf52c6c37
Kumara1973 <152588563+Kumara1973@users.noreply.github.com>
2026-02-01 17:39:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 let typeName = () => {
     let name = document.getElementById("textbox").value;
     document.getElementById("message").innerHTML = "Hi " + name;
+    document.getElementById("image").src = "https://www.shutterstock.com/image-vector/happy-birthday-greeting-card-clipart-600nw-2557598275.jpg";
+    document.getElementById("background").style.backgroundImage = "url(background.jpg)";
+    document.getElementById("background").style.backgroundRepeat = "no-repeat"
+    document.getElementById("background").style.backgroundSize = "100% 100%"
     
 }
 
-document.getElementById("bttn").addEventListener("click", typeName)
+let wishUser = () => {
+    let name = document.getElementById("textbox").value;
+    document.getElementById("message").innerHTML = "Merry Christmas " + name;
+    document.getElementById("image").src = "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/merry-christmas%2C-christmas-card-design-template-2283f3eacbb70fbcf77273fdcb2f7fd7_screen.jpg?ts=1670642657";
+    document.getElementById("background").style.backgroundImage = "url(https://thumbs.dreamstime.com/b/falling-snowflakes-winter-wonderland-sparkling-gently-fall-against-bright-blue-sky-creating-magical-scene-fresh-white-403877393.jpg)";
+    document.getElementById("background").style.backgroundRepeat = "no-repeat"
+    document.getElementById("background").style.backgroundSize = "100% 100%"
+}
+
+
+
+document.getElementById("bttn").addEventListener("click", typeName);
+document.getElementById("greet").addEventListener("click", wishUser);
